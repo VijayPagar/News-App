8a6ccc162a58b2c8ef77e318a025e2849ffb3519
Vijay <vijaypagar222@gmail.com>
2024-01-25 14:48:35 +0530

    new news-app project created

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import NavBar from './Components/NavBar';
+import News from './Components/News';
+
+function App() {
+  return (
+    <>
+    
+    <NavBar />
+    <News />
+    </>
+    
+
+    
+  );
+}
+
+export default App;
